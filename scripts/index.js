92eb0dc23b06d6fa122549a6f18e628bd7ac7e23
Ruslan Cheboksarov <badjokecat@yandex.ru>
2022-06-20 22:02:10 +0700

    fixed likes

--- scripts/index.js
+++ scripts/index.js
@@ -6,35 +6,25 @@ const popupProfileClose = document.querySelector('[name="popup__close-button-pro
 const addCardButton = document.querySelector('.profile__add-button');
 const popupCardClose = document.querySelector('[name="popup__close-button-addcard"]');
 //for popup profile
-const popupProfile = document.querySelector('#popup-profile');
+const popupProfile = document.querySelector('.popup_type_editprofile');
 const formProfileEdit = document.querySelector('[name="popupEditProfile"]');
 const popupProfileNameInput = document.querySelector('[name="popup__username"]');
 const popupProfileAboutInput = document.querySelector('[name="popup__userabout"]');
 const profName = document.querySelector('.profile__name');
 const profAbout = document.querySelector('.profile__about');
 //for popup adding cards
-const popupCard = document.querySelector('#popup-card');
+const popupCard = document.querySelector('.popup_type_addcard');
 const formCard = document.querySelector('[name="popupAddCard"]');
 const popupNameInputCard = document.querySelector('[name="popup__cardname"]');
 const popupLinkInputCard = document.querySelector('[name="popup__cardlink"]');
 const cardName = document.querySelector('.elements__title');
 const cardLink = document.querySelector('.elements__image');
 
-const popupPic = document.querySelector('.popup-pic');
+const popupPic = document.querySelector('.popup_type_image');
 const popupPicImage = document.querySelector('.popup-pic__image');
 const popupPicTitle = document.querySelector('.popup-pic__title');
 
 const popupPicCloseButton = popupPic.querySelector('[name="popup__close-button-image"]');
-
-// const photoGrid = document.querySelector('.elements');
-
-// let formSubmit = document.querySelector('.popup__form');
-
-
-// let nameInput = document.querySelector('.popup__input_data_name');
-// let jobInput = document.querySelector('.popup__input_data_about');
-// let profName = document.querySelector('.profile__name');
-// let profAbout = document.querySelector('.profile__about');
 
 const cards = document.querySelector('.elements');
 const gridCard = document.querySelector('#card').content;
@@ -47,7 +37,7 @@ function createCard(picName, picLink) {
     newCardImg.alt = picName;
     newCard.querySelector('.elements__title').textContent = picName;
 
-    newCard.querySelector('.elements__like-button').addEventListener('click', function (evt){
+    newCard.querySelector('.elements__like-button').addEventListener('click', function(evt){
         evt.target.classList.toggle('elements__like-button_active');
     });
 
